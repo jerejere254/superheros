dd1042ca1e1237d587fdbe5cb5ffac3c3166f3ba
JerryJohnsJuma <mtotongwat254@gmail.com>
2022-12-03 09:09:40 +0300

    seeding and migration

---
+++ app/models/hero_power.rb
@@ -0,0 +1,6 @@
+class HeroPower < ApplicationRecord
+    validates :strength, inclusion: {in: ["Strong", "Weak", "Average"]}
+
+    belongs_to :hero
+    belongs_to :power
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+STRENGTHS = ["Strong", "Weak", "Average"]
+
+puts " 😱Seeding spices..."
+
+10.times do
+seeded_heros = Hero.create(
+    name: Faker::Superhero.name,
+    super_name: Faker::Superhero.prefix
+)
+end
+
+10.times do
+    seeded_powers = Power.create!(
+        name: Faker::Superhero.power, 
+        description:  Faker::Lorem.sentence(word_count: 30))
+end
+
+30.times do
+    seeded_hero_powers = HeroPower.create!(strength: STRENGTHS.sample, hero_id: rand(1..10), power_id: rand(1..10))
+end
+
+puts " 😂Done seeding!"
